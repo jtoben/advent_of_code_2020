9ab3950c4fbc0efee339eef1d2be986aaa1e1a5a
Joost Toben <joost.toben@randstadgroep.nl>
2020-12-09 13:44:49 +0100

    Add solution to day 8.

---
+++ advent_of_code_2020-8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instructions = File.ReadAllLines("input.txt");
+            
+            Console.WriteLine(PartOne(instructions));
+            Console.WriteLine(PartTwo(instructions));
+        }
+
+        static int PartOne(string[] instructions) {
+            return RunCalculation(instructions).Accumulator;
+        }
+
+        static int PartTwo(string[] instructions) {
+            for (int i = 0; i < instructions.Length; i++) {
+
+                var copy = instructions.ToArray();
+                var instruction = copy[i];
+
+                switch (instruction.Split(' ')[0]) {
+                    case "nop":
+                        instruction = instruction.Replace("nop", "jmp");
+                        break;
+                    case "jmp":
+                        instruction = instruction.Replace("jmp", "nop");
+                        break;
+                    case "acc":
+                        continue;
+                }
+
+                copy[i] = instruction;
+
+                var result = RunCalculation(copy);
+                if (result.Success) {
+                    return result.Accumulator;
+                }
+
+            }
+            return -1;
+        }
+
+        static (bool Success, int Accumulator) RunCalculation(string[] instructions) {
+            int accumulator = 0;
+            int pointer = 0;
+
+            List<int> visitedInstructions = new List<int>();
+            bool ranSuccessFul = false;
+            while (true) {
+                if (visitedInstructions.Contains(pointer)) {
+                    break;
+                } 
+
+                if (pointer == instructions.Length) {
+                    ranSuccessFul = true;
+                    break;
+                }
+
+                visitedInstructions.Add(pointer);
+
+                var instruction = instructions[pointer].Split(' ')[0];
+                int parameter = int.Parse(instructions[pointer].Split(' ')[1]);
+
+                switch (instruction) {
+                    case "nop":
+                        pointer++;
+                        continue;
+                    case "acc":
+                        accumulator += parameter;
+                        pointer++;
+                        continue;
+                    case "jmp":
+                        pointer += parameter;
+                        continue;
+                }
+            }
+
+            return (ranSuccessFul, accumulator);
+        }
+    }
+}
